984ca8f88abe2925098c2f7cb7f5554f70b1e420
kctang <kctang@big2.net>
2017-12-14 18:59:58 +0800

    Update documentation

--- packages/graphql/src/ServiceInfo.ts
+++ packages/graphql/src/ServiceInfo.ts
@@ -1,8 +1,17 @@
 /**
- * ResolverService will
+ * ServiceInfo allows registration of services via ResolverService using a different name.
+ *
+ * By default, ResolverService.registerService() will register a service based on its class name.
+ *
+ * It is sometimes useful to override the class name. For example, "MongoSecurityService" is
+ * registered as "SecurityService".
  */
 export interface ServiceInfo {
-  info (): {
-    serviceName: string
-  }
+  /**
+   *
+   * Get service info object.
+   *
+   * @return {{serviceName: string}}
+   */
+  info (): { serviceName: string }
 }
